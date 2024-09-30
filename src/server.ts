a070def6c3e8a88023ae70c690433d011509eeee
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2024-09-30 22:37:34 +0200

    update server.ts to set mime types

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
+import path from "node:path";
 
 const app = express();
 const port: number = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript, Node.js, and Express!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
